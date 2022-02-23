4d5f2579ddac5a0758b5081f785ea4ad744a84a5
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-23 13:42:19 +0530

    Added:Created main class

--- Car.java
+++ Car.java
@@ -1,18 +1,22 @@
 package com.bridgelabz.Autoshop;
 
 public class Car {
-	private int speed;
+	private double speed;
 	private double regularPrice;
 	private String color;
-	public Car(int Speed,double regularPrice,String color)
+	public Car(double regularPrice2,double regularPrice,String color)
 	{
-		this.speed=Speed;
+		this.speed=regularPrice2;
 		this.regularPrice=regularPrice;
 		this.color=color;
 		}
 	public double getSalePrice(){
 		return regularPrice;
 		}
+	public String getRegularPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	}
 	
 	
--- MyownAutoshop.java
+++ MyownAutoshop.java
@@ -4,10 +4,13 @@ public class MyownAutoshop {
 
 	public static void main(String[] args) {
 		
-		Car c=new Car(0, 0, null);
-		c.getSalePrice();
-		
-		
-		
+		  Ford ford = new Ford(500, 500, "red", 30);
+	       Sedan sedan = new Sedan(300, 300, "black", 2021, 35);
+	        Truck truck = new Truck(250, 250, "yellow",  40);
+	        Car car = new Car(100, 100, "white");
+	        System.out.println("Ford "+ford.getSalePrice());
+	        System.out.println("Sedan "+sedan.getSalePrice());
+	        System.out.println("Truck "+truck.getSalePrice());
+	        System.out.println("car  "+car.getSalePrice());
+	    }
 	}
-}
