36c9bb0a14498331e12cbeeba21727da9b86013c
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-23 13:39:57 +0530

    Added:Created sub class

--- Truck.java
+++ Truck.java
@@ -1,28 +1,17 @@
 package com.bridgelabz.Autoshop;
 
 public class Truck extends Car{
+	 private int weight;
+
+		    public Truck(int speed, double regularPrice, String color, int weight) {
+		        super(speed, regularPrice, color);
+		        this.weight = weight;
+		    }
+
+		    public double getSalePrice() {
+		        return super.getSalePrice() * (weight > 2000 ? 0.85 : 0.75);
+		    }
+		}
+
 	
-
-private int weight = 0;
-public Truck (int Speed,double regularPrice,String color,int weight){
-	super(Speed, regularPrice, color);
-	this.weight=weight;
-}
-public double getSalePrice()
-
-	 {
-
-	  int regularPrice;
-	return (regularPrice - discount);
-	 {if(weight>2000){
-		 return super.getSalePrice()-(0.1*super.getSalePrice());
-		 }
-	 
-	 else{
-		 return super.getSalePrice();
-	 }
-	 }
-	 }
-
-	}
 
