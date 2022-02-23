b996c2b8c92c4d00398a2141042f6dacc74e93e6
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-23 08:00:20 +0530

    Added:Created sub class

---
+++ Ford.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Autoshop;
+
+public class Ford extends Car{
+
+	public Ford(int Speed, double regularPrice, String color) {
+		super(Speed, regularPrice, color);
+		// TODO Auto-generated constructor stub
+	}
+
+}
