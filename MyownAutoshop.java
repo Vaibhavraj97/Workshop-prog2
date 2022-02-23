d42ea94206481fbb458d6b5c5d620a03125e5c81
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-23 08:01:17 +0530

    Added:Created main class

---
+++ MyownAutoshop.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Autoshop;
+
+public class MyownAutoshop {
+
+	public static void main(String[] args) {
+		
+		Car c=new Car(0, 0, null);
+		c.getSalePrice();
+		
+		
+		
+	}
+}
