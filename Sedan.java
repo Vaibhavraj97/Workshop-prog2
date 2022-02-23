4324eb0497030a48a1dc79b74074aa853f4a4482
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-02-23 08:00:41 +0530

    Added:Created sub class

---
+++ Sedan.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Autoshop;
+
+public class Sedan extends Car {
+
+	public Sedan(int Speed, double regularPrice, String color) {
+		super(Speed, regularPrice, color);
+		// TODO Auto-generated constructor stub
+	}
+
+}
